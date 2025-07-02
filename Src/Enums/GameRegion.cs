35b43564f04c33fdb964214ca4d15e7f057c4dd1
Noah Wilber <ZNW4017@conagrafoods.com>
2025-07-02 09:27:26 -0500

    Added Register command with basic parrot operation

---
+++ Src/Enums/GameRegion.cs
@@ -0,0 +1,17 @@
+namespace HexStats.Enums;
+
+public enum GameRegion
+{
+    NA, // North America
+    EUW, // Europe West
+    EUNE, // Europe Nordic & East
+    KR, // Korea
+    CN, // China
+    SEA, // Southeast Asia
+    BR, // Brazil
+    LAN, // Latin America North
+    LAS, // Latin America South
+    OCE, // Oceania
+    TR, // Turkey
+    JP // Japan
+}
--- Src/Modules/GeneralModule.cs
+++ Src/Modules/GeneralModule.cs
@@ -1,4 +1,7 @@
+using System.ComponentModel;
+using Discord;
 using Discord.Interactions;
+using HexStats.Enums;
 
 namespace HexStats.Modules;
 
@@ -8,5 +11,22 @@ public class GeneralModule: InteractionModuleBase<SocketInteractionContext>
     public async Task PingCommandAsync()
     {
         await RespondAsync("Pong!", ephemeral: true);
+    }
+
+    [SlashCommand("register", "Register an account to be tracked by HexStats.")]
+    public async Task RegisterCommandAsync(
+        [Summary("Game Region", "The Region of the game account to register (e.g., NA, EUW, KR).")] GameRegion region,
+        [Summary("Username", "The username of the game account to register.")] string username,
+        [Summary("Tagline", "The tag of the game account to register (e.g., #1234).")] string tagline
+    )
+    {
+        var embed = new EmbedBuilder()
+            .WithTitle("Account Registered!")
+            .WithDescription(
+                $"Discord User: {Context.User.Username}#{Context.User.Discriminator} has been registered as Summoner: **{username}**:**{tagline}** in Region **{region}** and will now have their games tracked.")
+            .WithColor(Color.Green)
+            .Build();
+        
+        await RespondAsync(embed: embed, ephemeral: true);
     }
 }
